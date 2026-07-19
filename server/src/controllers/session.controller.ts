f7147a302b94571bc156f74f924d2fe31559dcf6
Yug <yugjain1646@gmail.com>
2026-07-19 16:13:58 +0530

    Sessions Service Started

--- server/src/controllers/session.controller.ts
+++ server/src/controllers/session.controller.ts
@@ -1,64 +1,37 @@
 import type { Response, Request } from "express";
+import {
+  deleteSessionById,
+  findSessionById,
+  findSessions,
+} from "../services/session.service.js";
 
 // TODO: Get all sessions
-export const getSessions = (req: Request, res: Response) => {
-  try {
-    //! GET SESSIONS SERVICE FUNCTION
-    res.status(200).json({ msg: "Sessions" });
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error !" });
-  }
+export const getSessions = async (req: Request, res: Response) => {
+  const sessions = await findSessions();
+  res.status(200).json(sessions);
 };
 
 // TODO: Get session by Id
-export const getSessionById = (req: Request, res: Response) => {
-  try {
-    //! GET SESSION BY ID SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
+export const getSessionById = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const session = await findSessionById(id);
+  res.status(200).json(session);
 };
 
-export const getSessionByUserId = (req: Request, res: Response) => {
-  try {
-    //! GET SESSION BY USER ID SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
+export const getSessionByUserId = async (req: Request, res: Response) => {};
 
 // TODO: Start a new Session
-export const createSession = (req: Request, res: Response) => {
-  try {
-    //! CREATE SESSION SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
+export const createSession = async (req: Request, res: Response) => {};
 
 // TODO: Update Session
-export const updateSession = (req: Request, res: Response) => {
-  try {
-    //! UPDATE SESSION SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
+export const updateSession = async (req: Request, res: Response) => {};
 
 // TODO: End Session
-export const endSession = (req: Request, res: Response) => {
-  try {
-    //! END SESSION SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
-};
+export const endSession = async (req: Request, res: Response) => {};
 
 // TODO: Delete Session
-export const deleteSession = (req: Request, res: Response) => {
-  try {
-    //! DELETE SESSION SERVICE FUNCTION
-  } catch (error) {
-    res.status(500).json({ msg: "Internal server error" });
-  }
+export const deleteSession = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const result = await deleteSessionById(id);
+  res.status(200).json(result);
 };
--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -1,22 +1,64 @@
 //* ----- Session Services -----
 
+import { db } from "../db/index.js";
+import { eq } from "drizzle-orm";
+import { sessions } from "../db/schema.js";
+import { AppError } from "../utils/AppError.js";
+
 //? GET All Sessions
-export const findSessions = () => {};
+export const findSessions = async () => {
+  const allSessions = await db.select().from(sessions);
+
+  if (allSessions.length === 0) {
+    throw new AppError(204, "No sessions found");
+  }
+  return allSessions;
+};
 
 //? GET Session by session id
-export const findSessionById = (id: number) => {};
+export const findSessionById = async (id: number) => {
+  const findSession = await db
+    .select()
+    .from(sessions)
+    .where(eq(sessions.id, id));
+
+  if (findSession.length === 0) {
+    throw new AppError(404, "Session not found");
+  }
+  return findSession;
+};
 
 //? GET Session by user id
-export const findSessionByUserId = (id: number) => {};
+export const findSessionByUserId = async (id: number) => {
+  const findSession = await db
+    // WE NEED TO CHECK IF USER IS VALID
+    .select()
+    .from(sessions)
+    .where(eq(sessions.userId, id));
+  if (findSession.length === 0) {
+    throw new AppError(404, "Session not found");
+  }
+  return findSession;
+};
 
 //? Create Session
-export const createSession = () => {};
-
-//? Update Session
-export const updateSession = (id: number) => {};
+export const createSession = async () => {};
 
 //? End Session
-export const endSession = (id: number) => {};
+export const endSession = async (id: number) => {};
+
+//? Update Session
+export const updateSessionById = async (id: number) => {};
 
 //? Delete Session
-export const deleteSession = (id: number) => {};
+export const deleteSessionById = async (id: number) => {
+  const deleted = await db
+    .delete(sessions)
+    .where(eq(sessions.id, id))
+    .returning({ id: sessions.id });
+
+  if (deleted.length === 0) {
+    throw new AppError(404, "Session not found");
+  }
+  return { msg: "Session deleted successfully" };
+};
